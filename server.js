72c50da26b42c8212755c6a036056c20041e8829
miriam-best-practice <nakiyingim@gmail.com>
2022-07-05 22:11:01 -0700

    add bordey parser and sin in route

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '123',
+      name: 'Mimi',
+      email: 'mimi@gmail.com',
+      pasword: 'banana',
+      entries: 0,
+      joined: new Date()
+    },
+  ]
+}
+
 app.get('/', (req, res) => {
   res.send('app is running')
-  console.log('yes')
+  
+})
+
+app.post('/signin', (req, res) => {
+  if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+    res.json('success');
+
+  }else {
+    res.status(400).json('error logging in')
+  }
 })
 
 app.listen(3000, ()=> {
