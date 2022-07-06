e5b024a1511e95785240bcaf0e8827c76e99ff4e
miriam-best-practice <nakiyingim@gmail.com>
2022-07-05 22:22:30 -0700

    add regiter route

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const database = {
       name: 'John',
       email: 'john@gmail.com',
       password: 'cookies',
-      entries: 0,
+      entries: 2,
       joined: new Date()
     },
     {
@@ -21,14 +21,14 @@ const database = {
       name: 'Mimi',
       email: 'mimi@gmail.com',
       pasword: 'banana',
-      entries: 0,
+      entries: 5,
       joined: new Date()
     },
   ]
 }
 
 app.get('/', (req, res) => {
-  res.send('app is running')
+  res.send(database.users)
   
 })
 
@@ -39,6 +39,19 @@ app.post('/signin', (req, res) => {
   }else {
     res.status(400).json('error logging in')
   }
+})
+
+app.post('/register', (req, res) => {
+  const {email, name, password} = req.body
+  database.users.push({
+    id: '126',
+    name: name,
+    email: email,
+    pasword: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.status(200).json(database.users[database.users.length - 1])
 })
 
 app.listen(3000, ()=> {
