8be464f33a04d6e15b7cad4d66800ce354641fe5
miriam-best-practice <nakiyingim@gmail.com>
2022-07-06 23:42:38 -0700

    add profile for the id

--- server.js
+++ server.js
@@ -82,16 +82,27 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const {id} = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if(user.id ===  id) {
-      found = true
-      return res.json(user)
+  db.select('*').from('users').where({
+    id: id
+  })
+  .then(user => {
+    if(user.length){
+      res.json(user[0])
+    } else {
+      res.status(400).json('Not found')
     }
   })
-  if(!found){
-    res.status(400).json('User not found')
-  }
+  .catch(err = res.status(400).json('user not found'))
+  // let found = false;
+  // database.users.forEach(user => {
+  //   if(user.id ===  id) {
+  //     found = true
+  //     return res.json(user)
+  //   }
+  // })
+  // if(!found){
+  //   res.status(400).json('User not found')
+  // }
 })
 
 app.put('/image',(req, res) => {
