905b30a78c0ae0600eabb5cbd2a05247be95914a
miriam-best-practice <nakiyingim@gmail.com>
2022-07-07 23:01:26 -0700

    add profile controller

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
 
 // initiating database
 const db = knex({
@@ -38,20 +39,7 @@ app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt
 app.post('/signin', (req, res)  => signin.handleSignin(req, res, db, bcrypt))
 
 // user profile end point
-app.get('/profile/:id', (req, res) => {
-  const {id} = req.params;
-  db.select('*').from('users').where({
-    id: id
-  })
-  .then(user => {
-    if(user.length){
-      res.json(user[0])
-    } else {
-      res.status(400).json('Not found')
-    }
-  })
-  .catch(err = res.status(400).json('user not found'))
-})
+app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
 
 
 // incrementing entries end point
